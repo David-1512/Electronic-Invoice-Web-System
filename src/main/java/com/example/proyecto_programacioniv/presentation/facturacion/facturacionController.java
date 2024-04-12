ef4a34453b95f03ba71088bf800eb7a659dd18f1
David-1512 <davidjserranom76@gmail.com>
2024-04-12 07:49:55 -0600

    Nuevo proyecto

---
+++ src/main/java/com/example/proyecto_programacioniv/presentation/facturacion/facturacionController.java
@@ -0,0 +1,111 @@
+package com.example.proyecto_programacioniv.presentation.facturacion;
+
+import com.example.proyecto_programacioniv.logic.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@org.springframework.stereotype.Controller("facturas")
+@RequestMapping("/presentation/facturacion")
+public class facturacionController {
+
+    @Autowired
+    private Service service;
+
+    private facturacionModel modelfacturacion;
+
+    public facturacionController(){
+        modelfacturacion = new facturacionModel();
+    }
+
+    @GetMapping("/")
+    public String show(Model model){
+        Optional<ProveedorEntity> proveedorOptional = service.proveedorFindById("86952");
+        if (proveedorOptional.isPresent()) {
+            ProveedorEntity proveedor = proveedorOptional.get();
+            model.addAttribute("nombreProveedor", proveedor.getNombre());
+            model.addAttribute(modelfacturacion.getTotalFactura());
+        } else {
+            model.addAttribute("nombreProveedor", "Proveedor no encontrado");
+        }
+        return "/presentation/facturacion/View";
+    }
+
+    @GetMapping("/searchCliente")
+    public String searchCliente(@RequestParam("clienteId") String clienteId,Model model,
+                                @RequestParam("nomProveedorCliente")String nomProveedor) {
+        Optional<ClienteEntity> clienteOptional = service.clienteFindById(clienteId);
+        if (clienteOptional.isPresent()) {
+            ClienteEntity cliente = clienteOptional.get();
+            model.addAttribute("nombreCliente", cliente.getNombre());
+            model.addAttribute("nombreProveedor", nomProveedor);
+        } else {
+            model.addAttribute("nombreCliente", "Cliente no encontrado");
+            model.addAttribute("nombreProveedor", nomProveedor);
+        }
+        return "presentation/facturacion/View";
+    }
+
+    @GetMapping("/searchProducto")
+    public String searchProducto(@RequestParam("codProducto") String cod,
+                                 @RequestParam("cantidad")Integer cantidad,
+                                 @RequestParam("nomProveedorProducto") String nomProveedor,
+                                 @RequestParam("nomClienteProducto") String nomCliente,
+                                 Model model) {
+        Optional<ProductoEntity> productoOptional = service.productoFindByCod(cod);
+        if (productoOptional.isPresent()) {
+            ProductoEntity producto = productoOptional.get();
+            LineaServicioEntity lineaServicioEntity = new LineaServicioEntity();
+            lineaServicioEntity.setCantidad(cantidad);
+            lineaServicioEntity.setProductoByCodProducto(producto);
+            model.addAttribute("nombreCliente", nomCliente);
+            model.addAttribute("nombreProveedor", nomProveedor);
+            modelfacturacion.agregarLineaServicio(lineaServicioEntity);
+            model.addAttribute("lineasServicios", modelfacturacion.findAll());
+            model.addAttribute("totalFactura",modelfacturacion.getTotalFactura());
+        }
+        return "presentation/facturacion/View";
+    }
+
+    @GetMapping("/disCantidad/{codigoP}")
+    public String disCantidad(Model model, @PathVariable int codigoP,
+                              @RequestParam("nomProveedorLista") String nomProveedor,
+                              @RequestParam("nomClienteLista") String nomCliente){
+        modelfacturacion.disminuirCantidad(codigoP);
+        model.addAttribute("nombreCliente", nomCliente);
+        model.addAttribute("nombreProveedor", nomProveedor);
+        model.addAttribute("lineasServicios", modelfacturacion.findAll());
+        model.addAttribute("totalFactura",modelfacturacion.getTotalFactura());
+        return "presentation/facturacion/View";
+    }
+
+    @GetMapping("/aumCantidad/{codigoP}")
+    public String aumCantidad(Model model, @PathVariable int codigoP,
+                              @RequestParam("nomProveedorLista") String nomProveedor,
+                              @RequestParam("nomClienteLista") String nomCliente){
+        modelfacturacion.aumentarCantidad(codigoP);
+        model.addAttribute("nombreCliente", nomCliente);
+        model.addAttribute("nombreProveedor", nomProveedor);
+        model.addAttribute("lineasServicios", modelfacturacion.findAll());
+        model.addAttribute("totalFactura",modelfacturacion.getTotalFactura());
+        return "presentation/facturacion/View";
+    }
+
+    @GetMapping("/eliminarProducto/{codigoP}")
+    public String eliminarProducto(Model model, @PathVariable int codigoP,
+                              @RequestParam("nomProveedorLista") String nomProveedor,
+                              @RequestParam("nomClienteLista") String nomCliente){
+        modelfacturacion.eliminarProducto(codigoP);
+        model.addAttribute("nombreCliente", nomCliente);
+        model.addAttribute("nombreProveedor", nomProveedor);
+        model.addAttribute("lineasServicios", modelfacturacion.findAll());
+        model.addAttribute("totalFactura",modelfacturacion.getTotalFactura());
+        return "presentation/facturacion/View";
+    }
+}
+
---
+++ src/main/java/com/example/proyecto_programacioniv/presentation/facturacion/facturacionModel.java
@@ -0,0 +1,67 @@
+package com.example.proyecto_programacioniv.presentation.facturacion;
+
+import com.example.proyecto_programacioniv.logic.LineaServicioEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class facturacionModel {
+
+    private List<LineaServicioEntity> listLinea;
+
+    private int cont;
+
+    private double totalFactura;
+
+    public facturacionModel(){
+        listLinea = new ArrayList<>();
+        cont = 1;
+        totalFactura = 0.0;
+    }
+
+    public void agregarLineaServicio(LineaServicioEntity linea){
+        linea.setCod(cont);
+        linea.setSubtotal(subtotal(linea));
+        totalFactura += subtotal(linea);
+        listLinea.add(linea);
+        cont++;
+    }
+
+    public List<LineaServicioEntity> findAll(){return listLinea;}
+
+    public void disminuirCantidad(int cod){
+        LineaServicioEntity result = listLinea.stream()
+                .filter(i->i.getCod() == cod).findFirst().orElse(null);
+        if(result.getCantidad() != 1) {
+            result.setCantidad(result.getCantidad() - 1);
+            result.setSubtotal(subtotal(result));
+            totalFactura -= result.getProductoByCodProducto().getPrecio();
+        }
+    }
+
+    public void aumentarCantidad(int cod){
+        LineaServicioEntity result = listLinea.stream()
+                .filter(i->i.getCod() == cod).findFirst().orElse(null);
+        if(result.getCantidad() != 1) {
+            result.setCantidad(result.getCantidad() + 1);
+            result.setSubtotal(subtotal(result));
+            totalFactura += result.getProductoByCodProducto().getPrecio();
+        }
+    }
+
+    public void eliminarProducto(int cod){
+        LineaServicioEntity result = listLinea.stream()
+                .filter(i->i.getCod() == cod).findFirst().orElse(null);
+            listLinea.remove(result);
+        totalFactura -= subtotal(result);
+            cont--;
+
+    }
+
+    private int subtotal(LineaServicioEntity linea){  //Cambiar a double
+        int subtotal = linea.getCantidad() * linea.getProductoByCodProducto().getPrecio();
+        return subtotal;
+    }
+
+    public double getTotalFactura(){return totalFactura;}
+}
