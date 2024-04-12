0fac3e259626e7383f5927c9ee8fbdfd2af41dbc
XIARA <xiarasualpi@gmail.com>
2024-04-11 23:08:59 -0600

    Union administracion y Productos

--- src/main/java/com/example/proyecto_programacioniv/data/ProveedorRepository.java
+++ src/main/java/com/example/proyecto_programacioniv/data/ProveedorRepository.java
@@ -3,6 +3,7 @@ import com.example.proyecto_programacioniv.logic.ProductoEntity;
 import com.example.proyecto_programacioniv.logic.ProveedorEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProveedorRepository extends CrudRepository<ProveedorEntity,String> {
+import java.util.List;
 
+public interface ProveedorRepository extends CrudRepository<ProveedorEntity,String> {//nombre, y el tipo que es la PK
 }
